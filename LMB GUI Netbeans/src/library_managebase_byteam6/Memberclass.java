abfea638d1f5c4a9b29aac2d358ca3edbd26e539
amrina rossada <amrina0612@gmail.com>
2023-06-16 08:19:54 +0800

    membuat class

---
+++ LMB GUI Netbeans/src/library_managebase_byteam6/Memberclass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package library_managebase_byteam6;
+
+/**
+ *
+ * @author USER
+ */
+public class Memberclass {
+    
+}
